bbb9bbca718951901838807af111f9475c26c716
Sampkeman211 <sampleman211@gmail.com>
2023-07-06 10:30:22 +0300

    VoiceAsis1.2
    VoiceAsis1.2

---
+++ main.py
@@ -0,0 +1,19 @@
+import json, pyaudio
+from vosk import Model, KaldiRecognizer
+
+model = Model('osk-model-small-ru-0.4')
+rec = KaldiRecognizer(model, 44100)
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+def listen():
+    while True:
+        data = stream.read(4000, exception_on_overflow=False)
+        if (rec.AcceptWaveform(data)) and (len(data) > 0):
+            answer = json.loads(rec.Result())
+            if answer['text']:
+                yield answer['text']
+
+for text in listen():
+    print(text)
